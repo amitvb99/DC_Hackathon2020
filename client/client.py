8f58e1237cf34a457fcfa54781e99e45b178155b
Amit Vigdor Bart <amitvigdorbart@gmail.com>
2020-12-27 21:22:56 +0200

    almost finished

--- client/client.py
+++ client/client.py
@@ -1,33 +1,65 @@
 import socket
 from pynput.keyboard import Listener
 import platform
+import select
 
-client_TCP_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
+client_TCP_socket = None
 
 
 def on_press(key):
-    client_TCP_socket.send(key.char.encode('utf-8'))
+    if client_TCP_socket is not None:
+        client_TCP_socket.send(key.char.encode('utf-8'))
 
 
 if platform.system() == 'Windows':
     socket.SO_REUSEPORT = socket.SO_REUSEADDR
 
-client_UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
-client_UDP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-client_UDP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-client_UDP_socket.bind(("", 13117))
-
 while True:
+    # init sockets
+    client_TCP_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
+    client_UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    client_UDP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    client_UDP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    client_UDP_socket.bind(("", 13117))
+    print('Client started, listening for offer requests...​”.')
+
     data, addr = client_UDP_socket.recvfrom(1024)
-    print(f"Received offer from " + addr[0] + " attempting to connect...")
+    data = data.decode('utf-8')
+    magic, message_type, port = data[2:10], data[10:12], data[12:16]
+    if magic == 'feedbeef' and message_type == '02':
+        print(f"Received offer from " + addr[0] + " attempting to connect...")
+        client_TCP_socket.connect((addr[0], int(port, 16)))
+        client_UDP_socket.close()
 
-    client_TCP_socket.connect((addr[0], 12345))
-    try:
-        while True:
+        """ selecting team name """
+        data, addr = client_TCP_socket.recvfrom(1024)
+        data = data.decode('utf-8')
+        name = input(data)
+        client_TCP_socket.send(name.encode('utf-8'))
+
+        """ welcome message """
+        data, addr = client_TCP_socket.recvfrom(1024)
+        data = data.decode('utf-8')
+        print(data)
+
+        """ game starts """
+        try:
             with Listener(on_press=on_press) as listener:
-                listener.join()
-    except KeyboardInterrupt:
-        print('Exit by the user')
+                while True:
+                    ready = select.select([client_TCP_socket], [], [], 0.2)
+                    data = None
+                    if ready[0]:
+                        data = client_TCP_socket.recv(1024).decode('utf-8')
+                    if data is not None:
+                        listener.stop()
+                        break
 
-    client_TCP_socket.close()
+        except KeyboardInterrupt:
+            print('Exit by the user')
 
+        """ game over message """
+        # data, addr = client_TCP_socket.recvfrom(1024)
+        # data = data.decode('utf-8')
+        print(data)
+
+        client_TCP_socket.close()
--- server/server.py
+++ server/server.py
@@ -2,9 +2,20 @@ import socket
 import time
 import threading
 import platform
-from pynput.keyboard import Listener
+import random
+import select
 
 thread_count = 0
+players_ready = 0
+players_limit = 2
+groups = {'Group 1': [], 'Group 2': []}
+shuffle_ready = False
+game_over = False
+teams = {}
+letters = {}
+lock = threading.Lock()
+restart_game = False
+broadcast = True
 
 
 class UdpThread (threading.Thread):
@@ -19,12 +30,18 @@ class UdpThread (threading.Thread):
         server_socket_udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         # Enable broadcasting mode
         server_socket_udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        message = b'127.0.0.1'  # TODO: change to real message
+        magic_cookie = 0xfeedbeef
+        message_type = 0x2
+        tcp_port = 12345
+        message = bytes(hex(tcp_port + (16**4)*message_type + (16**6)*magic_cookie), 'utf-8')
         server_socket_udp.settimeout(0.2)
-        while True:
+        while broadcast:
             server_socket_udp.sendto(message, ('<broadcast>', 13117))
-            print("message sent!")
             time.sleep(1)
+        server_socket_udp.close()
+
+    def kill(self):
+        raise SystemExit()
 
 
 class TcpThread (threading.Thread):
@@ -34,24 +51,129 @@ class TcpThread (threading.Thread):
         self.client_socket = client_socket
 
     def run(self):
-        print('tcp thread')
-        while True:
-            data = self.client_socket.recv(1024)
-            if not data or data.decode('utf-8') == 'END':
-                break
-            print('received from client : %s' % data.decode('utf-8'))
+        global players_ready, groups
+        self.client_socket.send('Please enter your team name: '.encode('utf-8'))
+        name = self.client_socket.recv(1024).decode('utf-8')
+        register_team(name)
+        while not shuffle_ready:
+            continue
+        message = """
+        Welcome to Keyboard Spamming Battle Royale.\n
+        Group 1:\n
+        ==\n"""
+        for team in groups['Group 1']:
+            message += team + '\n'
+        message += """
+        Group 2:\n
+        ==\n"""
+        for team in groups['Group 2']:
+            message += '\t' + team + '\n'
+        message += 'Start pressing keys on your keyboard as fast as you can!!\n'
+        self.client_socket.send(message.encode('utf-8'))
+
+        while not game_over:
+            ready = select.select([self.client_socket], [], [], 0.2)
+            data = None
+            if ready[0]:
+                data = self.client_socket.recv(1024).decode('utf-8')
+            if data is None:
+                continue
+            print('received from client : %s' % data)
+            lock.acquire()
+            teams[name] += 1
+            if data in letters.keys():
+                letters[data] += 1
+            else:
+                letters[data] = 1
+            lock.release()
+
+        print('game over')
+        group1_score = sum([teams[team] for team in groups['Group 1']])
+        group2_score = sum([teams[team] for team in groups['Group 2']])
+        if group2_score < group1_score:
+            winner = 'Group 1'
+        else:
+            winner = 'Group 2'
+
+        message = f"""
+        Game Over!\n
+        Group 1 typed in {group1_score} characters, Group 2 typed in {group2_score} characters.\n
+        {winner} wins!\n
+        \n
+        Congratulations to the winners:
+        """
+        for team in groups[winner]:
+            message += team + '\n'
+
+        client_socket.send(message.encode('utf-8'))
+        print(message)
         self.client_socket.close()
+
+    def kill(self):
+        raise SystemExit()
+
+
+def register_team(name):
+    print('registering')
+    global players_ready
+    lock.acquire()
+    teams[name] = 0
+    players_ready += 1
+    print(players_ready)
+
+    lock.release()
+
+
+def reset_game():
+    global thread_count, players_ready, players_limit, groups, shuffle_ready, game_over, teams, letters
+    thread_count = 0
+    players_ready = 0
+    players_limit = 2
+    # groups = {'Group 1': [], 'Group 2': []}
+    shuffle_ready = False
+    # game_over = False
+    # teams = {}
+    # letters = {}
 
 
 if __name__ == '__main__':
-    udp = UdpThread()
-    udp.start()
-    print("Server started,listening on ip 192.168.56.1")
-    server_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket_tcp.bind(('192.168.56.1', 12345))
-    print('TCP socket wait for connection')
-    server_socket_tcp.listen(4)
     while True:
-        client_socket, addr = server_socket_tcp.accept()
-        tcp = TcpThread(client_socket)
-        tcp.start()
+        reset_game()
+        udp = UdpThread()
+        udp.start()
+        print("Server started,listening on ip 192.168.56.1")
+        server_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket_tcp.bind(('192.168.56.1', 12345))
+        print('TCP socket wait for connection')
+        server_socket_tcp.listen(4)
+        while True:
+            if thread_count != players_limit:
+                client_socket, addr = server_socket_tcp.accept()
+                if restart_game:
+                    restart_game = False
+                    groups = {'Group 1': [], 'Group 2': []}
+                    game_over = False
+                    teams = {}
+                    letters = {}
+                tcp = TcpThread(client_socket)
+                tcp.start()
+                lock.acquire()
+                thread_count += 1
+                lock.release()
+
+            if players_ready == players_limit:
+                broadcast = False
+                lst = list(teams.keys())
+                random.shuffle(lst)
+                groups['Group 1'], groups['Group 2'] = lst[:int(len(lst) / 2)], lst[int(len(lst) / 2):]
+                print(groups)
+                shuffle_ready = True
+                start_time = time.time()
+                while time.time() < start_time + 10:
+                    continue
+                game_over = True
+                break
+
+        print('​Game over, sending out offer requests...')
+        restart_game = True
+        broadcast = True
