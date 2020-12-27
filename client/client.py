c3d86954b313b88523cb208b1943439a26fd660f
Amit Vigdor Bart <amitvigdorbart@gmail.com>
2020-12-27 16:36:40 +0200

    server and client connecting!!!! (tcp & udp)

--- client/client.py
+++ client/client.py
@@ -1,5 +1,6 @@
 import socket
 from pynput.keyboard import Listener
+import platform
 
 client_TCP_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
 
@@ -8,16 +9,19 @@ def on_press(key):
     client_TCP_socket.send(key.char.encode('utf-8'))
 
 
+if platform.system() == 'Windows':
+    socket.SO_REUSEPORT = socket.SO_REUSEADDR
+
 client_UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
 client_UDP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 client_UDP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 client_UDP_socket.bind(("", 13117))
 
 while True:
-    # data, addr = client_UDP_socket.recvfrom(1024)
-    # print(f"Received offer from" + addr[0] + " attempting to connect...")
+    data, addr = client_UDP_socket.recvfrom(1024)
+    print(f"Received offer from " + addr[0] + " attempting to connect...")
 
-    client_TCP_socket.connect(('127.0.0.1', 12345))
+    client_TCP_socket.connect((addr[0], 12345))
     try:
         while True:
             with Listener(on_press=on_press) as listener:
--- server/server.py
+++ server/server.py
@@ -1,40 +1,57 @@
 import socket
 import time
-from _thread import *
-
-thread_count = 0
+import threading
+import platform
 from pynput.keyboard import Listener
 
-
-def udp_socket():
-    pass
+thread_count = 0
 
 
-def TCP_CLIENT(client_socket):
-    pass
+class UdpThread (threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        if platform.system() == 'Windows':
+            socket.SO_REUSEPORT = socket.SO_REUSEADDR
+        server_socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        server_socket_udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        # Enable broadcasting mode
+        server_socket_udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        message = b'127.0.0.1'  # TODO: change to real message
+        server_socket_udp.settimeout(0.2)
+        while True:
+            server_socket_udp.sendto(message, ('<broadcast>', 13117))
+            print("message sent!")
+            time.sleep(1)
+
+
+class TcpThread (threading.Thread):
+
+    def __init__(self, client_socket):
+        threading.Thread.__init__(self)
+        self.client_socket = client_socket
+
+    def run(self):
+        print('tcp thread')
+        while True:
+            data = self.client_socket.recv(1024)
+            if not data or data.decode('utf-8') == 'END':
+                break
+            print('received from client : %s' % data.decode('utf-8'))
+        self.client_socket.close()
 
 
 if __name__ == '__main__':
-    # server_socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    # server_socket_udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    # # Enable broadcasting mode
-    # server_socket_udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    # message = b'127.0.0.1'  #TODO change to real message
-    # server_socket_udp.settimeout(0.2)
-    # while True:
-    #     server_socket_udp.sendto(message, ('<broadcast>', 13117))
-    #     print("message sent!")
-    #     time.sleep(1)
-
-    print("Server started,listening on ip 127.0.0.1")
+    udp = UdpThread()
+    udp.start()
+    print("Server started,listening on ip 192.168.56.1")
     server_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket_tcp.bind(('127.0.0.1', 12345))
+    server_socket_tcp.bind(('192.168.56.1', 12345))
     print('TCP socket wait for connection')
     server_socket_tcp.listen(4)
-    client_socket, addr = server_socket_tcp.accept()
     while True:
-        data = client_socket.recv(1024)
-        if not data or data.decode('utf-8') == 'END':
-            break
-        print('received from client : %s' % data.decode('utf-8'))
-    client_socket.close()
+        client_socket, addr = server_socket_tcp.accept()
+        tcp = TcpThread(client_socket)
+        tcp.start()
